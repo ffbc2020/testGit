21c9406eea0bb7f748c1e4e89d1717cd95e4dfc7
eclipse_fyh <eclipse_email@126.com>
2020-02-09 20:47:09 +0800

    My first commit

---
+++ src/testGit/ShuChu.java
@@ -0,0 +1,14 @@
+package testGit;
+
+public class ShuChu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+		
+		Sum sum = new Sum();
+		sum.qiuHe();
+	}
+
+}
---
+++ src/testGit/Sum.java
@@ -0,0 +1,9 @@
+package testGit;
+
+public class Sum {
+	public void qiuHe() {
+		
+		System.out.print("«Û∫Õº∆À„!");
+	}
+
+}
